ba788a1aaff56f1ecb4ee0a6e84485591797e577
Pedro Jorge De Los Santos <delossantosmfq@gmail.com>
2023-05-04 15:03:07 -0600

    first commit webserver

---
+++ app.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express')
+const hbs = require('hbs');
+
+
+const app = express()
+const port = process.env.PORT;
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Servir contenido estático
+app.use( express.static('public') );
+
+app.get('/', function (req, res) {
+  res.render('home', {
+    nombre: 'Jorge',
+    titulo: 'Curso de Node'
+  });
+});
+
+app.get('/generic', function (req, res) {
+  res.render('generic', {
+    nombre: 'Jorge',
+    titulo: 'Curso de Node'
+  });
+});
+
+app.get('/elements', function (req, res) {
+  res.render('elements', {
+    nombre: 'Jorge',
+    titulo: 'Curso de Node'
+  });
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/public/404.html')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
